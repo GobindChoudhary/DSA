4ba6a71d2e5f18d714fb0d1ffe3451e9cb43d370
GobindChoudhary <chaudharygobind24@gmail.com>
2025-07-05 14:21:01 +0530

    Insertion sort

--- sorting.js
+++ sorting.js
@@ -15,17 +15,29 @@ let arr = [32, 41, 1, 90, 55, 83, 88, 23, 49, 57];
 
 // Selection sort
 
-for (let i = 0; i < arr.length - 1; i++) {
-  let small = i;
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[small] > arr[j]) {
-      small = j;
-    }
+// for (let i = 0; i < arr.length - 1; i++) {
+//   let small = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[small] > arr[j]) {
+//       small = j;
+//     }
+//   }
+//   if (small != i) {
+//     let temp = arr[small];
+//     arr[small] = arr[i];
+//     arr[i] = temp;
+//   }
+// }
+// console.log(arr);
+
+// Insertion sort
+
+for (let i = 1; i < arr.length; i++) {
+  let key = arr[i];
+  let j = i - 1;
+  for (; j >= 0 && key < arr[j]; j--) {
+    arr[j + 1] = arr[j];
   }
-  if (small != i) {
-    let temp = arr[small];
-    arr[small] = arr[i];
-    arr[i] = temp;
-  }
+  arr[j + 1] = key;
 }
 console.log(arr);
