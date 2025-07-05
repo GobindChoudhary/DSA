18b1dd1c95cf9d50ebe85157ee4a061e7d853405
GobindChoudhary <chaudharygobind24@gmail.com>
2025-07-05 14:24:13 +0530

    Insertion sort

--- sorting.js
+++ sorting.js
@@ -32,12 +32,12 @@ let arr = [32, 41, 1, 90, 55, 83, 88, 23, 49, 57];
 
 // Insertion sort
 
-for (let i = 1; i < arr.length; i++) {
-  let key = arr[i];
-  let j = i - 1;
-  for (; j >= 0 && key < arr[j]; j--) {
-    arr[j + 1] = arr[j];
-  }
-  arr[j + 1] = key;
-}
-console.log(arr);
+// for (let i = 1; i < arr.length; i++) {
+//   let key = arr[i];
+//   let j = i - 1;
+//   for (; j >= 0 && key < arr[j]; j--) {
+//     arr[j + 1] = arr[j];
+//   }
+//   arr[j + 1] = key;
+// }
+// console.log(arr);
