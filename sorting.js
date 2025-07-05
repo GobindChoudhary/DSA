b9e7ba22626fd2f9e81c4368cf2cdbc5fc9ce7ba
GobindChoudhary <chaudharygobind24@gmail.com>
2025-07-05 12:55:47 +0530

    bubble Short

---
+++ sorting.js
@@ -0,0 +1,14 @@
+// bubble Short
+
+let arr = [32, 41, 1, 90, 55, 83, 88, 23, 49, 57];
+
+for (let i = 0; i < arr.length - 1; i++) {
+  for (let j = 0; j < arr.length - i - 1; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+console.log(arr);
