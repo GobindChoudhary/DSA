21624dd5edb28eadb102ddfe283380c43220ff0b
GobindChoudhary <chaudharygobind24@gmail.com>
2025-07-06 12:06:54 +0530

    Inserting element in mutidimentional array

--- MultiDimentionalArray.js
+++ MultiDimentionalArray.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")();
+
 // Traversal throught multidimentionla array
 
 const arr = Array.from({ length: 3 }, () => {
@@ -10,3 +12,18 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 console.log(arr);
+
+// inserting element in mutidimentional array
+
+const size = Number(prompt("Enter the size of the array: "));
+const arr1 = new Array(size);
+for (let i = 0; i < arr1.length; i++) {
+  const innerArraysize = +prompt(`Enter the size array at index ${i}: `);
+  arr1[i] = new Array(innerArraysize);
+  for (let j = 0; j < arr1[i].length; j++) {
+    const element = +prompt(`Enter ${j}th index element `);
+    arr1[i][j] = element;
+  }
+}
+
+console.log(arr1);
